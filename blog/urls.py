84cdc034ec308f70259fbe3beb1204997d9fce37
bennys-quarter <benedikt.privat@hotmail.com>
2025-09-06 20:22:49 +0200

    STEP adding template and static files

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
 
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
 
